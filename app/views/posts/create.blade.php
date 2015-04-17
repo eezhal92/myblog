839f5e064da4bf87cd9288b3cd08b5be40e7c859
adiatma <adiyatmakamarudin@gmail.com>
2015-04-17 23:34:20 +0800

    add old input

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -2,11 +2,13 @@
 	{{ Form::token() }}
 	<div>
 		<label>Title</label>
-		<input type="text" name="title">
+		<input type="text" name="title" value="@if(Input::old('title')){{Input::old('title')}}@endif">
+		@if($errors->first('title')) <span>{{ $errors->first('title') }}</span> @endif
 	</div>
 	<div>
 		<label>Body</label>
-		<textarea></textarea>		
+		<textarea name="body">@if(Input::old('body')){{Input::old('body')}}@endif</textarea>
+		@if($errors->first('body')) <span>{{ $errors->first('body') }}</span> @endif			
 	</div>
 	<div>
 		<input type="submit" value="save">
