4246452db112b06d9f504d84b80ecda259fe98d5
adiatma <adiyatmakamarudin@gmail.com>
2015-04-18 00:29:51 +0800

    add create link on index

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -1,5 +1,6 @@
 <h1>INI INDEX NYA POSTS </h1>
-
+{{ link_to_route('posts.create', 'Tambah') }}
+<div>
 @foreach($artikel as $a)
   {{ $a->title }}<br>
   {{ $a->body }} <br>
@@ -12,3 +13,4 @@
   </div>
  <hr>	
 @endforeach
+</div>
